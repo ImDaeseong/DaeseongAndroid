3b036314673995d7d102775461415ab465d4a082
ImDaeseong <cs93059@gmail.com>
2018-03-26 11:48:24 +0900

    DaeseongAndroid

---
+++ Toast_test/app/src/main/java/com/im/daeseong/toast_test/MainActivity.java
@@ -0,0 +1,95 @@
+package com.im.daeseong.toast_test;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button1, button2, button3;
+    private ImageView ivbitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivbitmap = (ImageView)findViewById(R.id.ivbitmap);
+
+        button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LayoutInflater inflater = getLayoutInflater();
+                View view = inflater.inflate(R.layout.toast_layout, (ViewGroup)findViewById(R.id.toastlayout));
+                TextView tvtoast = view.findViewById(R.id.tvtoast);
+                tvtoast.setText("Toast test");
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.BOTTOM, 0, 0);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(view);
+                toast.show();
+
+            }
+
+        });
+
+        button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Snackbar.make(v, "Snackbar test", Snackbar.LENGTH_LONG).show();
+
+            }
+        });
+
+        button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bitmap bitmap = getBitmapFromView(getWindow().getDecorView());
+                if(bitmap != null){
+                    ivbitmap.setImageBitmap(bitmap);
+                }
+            }
+        });
+    }
+
+    private Bitmap getBitmapFromView(View view){
+        try {
+            Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(returnedBitmap);
+            Drawable bgDrawable = view.getBackground();
+            if (bgDrawable != null)
+                bgDrawable.draw(canvas);
+            else
+                canvas.drawColor(Color.WHITE);
+            view.draw(canvas);
+            return returnedBitmap;
+        }catch (Exception e){
+        }
+        return null;
+    }
+
+}
