b8f83b1c18e564cf3bb72ca73bda75aa14b93ff9
ImDaeseong <cs93059@gmail.com>
2022-07-01 10:47:50 +0900

    DaeseongAndroid

--- EncryptedSharedPreference_test/app/src/main/java/com/daeseong/encryptedsharedpreference_test/util/encryptedsharedpreference_util.java
+++ EncryptedSharedPreference_test/app/src/main/java/com/daeseong/encryptedsharedpreference_test/util/encryptedsharedpreference_util.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import androidx.security.crypto.EncryptedSharedPreferences;
 import androidx.security.crypto.MasterKeys;
-
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Map;
