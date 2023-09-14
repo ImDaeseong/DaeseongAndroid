7c7e07f9e242d217be821420dd39196405b1b91a
ImDaeseong <cs93059@gmail.com>
2023-09-14 14:14:01 +0900

    DaeseongAndroid

--- AnimateSlide_test/app/src/main/java/com/im/daeseong/animateslide_test/SecondActivity.java
+++ AnimateSlide_test/app/src/main/java/com/im/daeseong/animateslide_test/SecondActivity.java
@@ -1,6 +1,6 @@
 package com.im.daeseong.animateslide_test;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SecondActivity extends AppCompatActivity {
