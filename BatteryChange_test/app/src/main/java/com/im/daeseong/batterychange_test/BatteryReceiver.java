16e0e0d8cd61caf95b5aefd13bedf8e981d82393
ImDaeseong <cs93059@gmail.com>
2023-09-15 11:56:07 +0900

    DaeseongAndroid

--- BatteryChange_test/app/src/main/java/com/im/daeseong/batterychange_test/BatteryReceiver.java
+++ BatteryChange_test/app/src/main/java/com/im/daeseong/batterychange_test/BatteryReceiver.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class BatteryReceiver extends BroadcastReceiver{
+public class BatteryReceiver extends BroadcastReceiver {
 
     public BatteryListener batteryListener;
 
