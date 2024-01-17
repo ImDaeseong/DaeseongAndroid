d62c5dbd5b63ca97ad3732c8426e88b5e0657a65
ImDaeseong <cs93059@gmail.com>
2024-01-17 15:08:27 +0900

    DaeseongAndroid

---
+++ COIL_test/app/src/main/java/com/daeseong/coil_test/Coil3Activity.java
@@ -0,0 +1,37 @@
+package com.daeseong.coil_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import coil.Coil;
+import coil.request.ImageRequest;
+import coil.transform.GrayscaleTransformation;
+
+public class Coil3Activity extends AppCompatActivity {
+
+    private String sPngUrl = "https://cdn.pixabay.com/photo/2015/07/14/18/14/school-845196_960_720.png";
+
+    private ImageView iv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coil3);
+
+        iv1 = findViewById(R.id.iv1);
+
+        loadImage();
+    }
+
+    private void loadImage() {
+
+        ImageRequest request = new ImageRequest.Builder(this)
+                .data(sPngUrl)
+                .target(iv1)
+                .transformations(new GrayscaleTransformation())
+                .build();
+
+        Coil.imageLoader(this).enqueue(request);
+    }
+
+}
---
+++ COIL_test/app/src/main/java/com/daeseong/coil_test/MainActivity.java
@@ -0,0 +1,46 @@
+package com.daeseong.coil_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button1, button2, button3, button4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        button4 = (Button)findViewById(R.id.button4);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button1:
+                startActivity(new Intent(this, Coil1Activity.class));
+                break;
+            case R.id.button2:
+                startActivity(new Intent(this, Coil2Activity.class));
+                break;
+            case R.id.button3:
+                startActivity(new Intent(this, Coil3Activity.class));
+                break;
+            case R.id.button4:
+                startActivity(new Intent(this, Coil4Activity.class));
+                break;
+        }
+    }
+}
