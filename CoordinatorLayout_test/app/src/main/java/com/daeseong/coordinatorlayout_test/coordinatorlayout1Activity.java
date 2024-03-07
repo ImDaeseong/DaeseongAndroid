7a9adfc853255a3001c7a468806b3bc62b8313a5
ImDaeseong <cs93059@gmail.com>
2024-03-07 12:09:17 +0900

    DaeseongAndroid

---
+++ CoordinatorLayout_test/app/src/main/java/com/daeseong/coordinatorlayout_test/coordinatorlayout1Activity.java
@@ -0,0 +1,22 @@
+package com.daeseong.coordinatorlayout_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class coordinatorlayout1Activity extends AppCompatActivity {
+
+    private WebView web1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordinatorlayout1);
+
+        web1 = (WebView)findViewById(R.id.web1);
+        web1.getSettings().setJavaScriptEnabled(true);
+        web1.setWebViewClient(new WebViewClient());
+        web1.loadUrl("https://m.naver.com/");
+    }
+}
