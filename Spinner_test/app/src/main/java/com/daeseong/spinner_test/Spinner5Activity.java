d956c709c3d7e848cd6f99fa2325ea7012dce332
ImDaeseong <cs93059@gmail.com>
2024-03-18 18:52:24 +0900

    DaeseongAndroid

---
+++ Spinner_test/app/src/main/java/com/daeseong/spinner_test/Spinner5Activity.java
@@ -0,0 +1,16 @@
+package com.daeseong.spinner_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Spinner5Activity extends AppCompatActivity {
+
+    private static final String TAG = Spinner5Activity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner5);
+
+    }
+}
