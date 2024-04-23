93e77148b4ea5631481e7769911e3a910ec4023f
ImDaeseong <cs93059@gmail.com>
2024-04-23 16:27:23 +0900

    DaeseongAndroid

---
+++ CoordinatorLayout_test/app/src/main/java/com/daeseong/coordinatorlayout_test/coordinatorlayout5Activity.java
@@ -0,0 +1,14 @@
+package com.daeseong.coordinatorlayout_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class coordinatorlayout5Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordinatorlayout5);
+    }
+}
